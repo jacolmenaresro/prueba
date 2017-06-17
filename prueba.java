c2096c71019d7d14b006097631cce128f37e8e30
carlos <carlosroberto.rojas@gmail.com>
2017-06-16 19:27:02 -0500

    commit inical

---
+++ prueba.java
@@ -0,0 +1,10 @@
+public class Deposito {    
+
+    //Campos de la clase
+    private float diametro;
+    private float altura;
+    private String idDeposito;
+
+    //Constructor sin parámetros auxiliar
+    public Deposito () { //Lo que hace es llamar al constructor con parámetros pasándole valores vacíos
+        this(0,0,"");            }
